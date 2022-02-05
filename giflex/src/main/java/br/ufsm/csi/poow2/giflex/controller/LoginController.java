57f10ce3a2459e10c7d938193e4834a9cf07dfc7
nathlia <nattyfelfer@gmail.com>
2022-02-05 00:08:56 -0300

    added customized AuthenticationManager

---
+++ giflex/src/main/java/br/ufsm/csi/poow2/giflex/controller/LoginController.java
@@ -0,0 +1,39 @@
+package br.ufsm.csi.poow2.giflex.controller;
+
+import br.ufsm.csi.poow2.giflex.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> auth(@RequestBody Player player) {
+        System.out.println("Username: " + player.getUsername());
+
+        try {
+            final Authentication authenticaticon = this.authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(player.getUsername(), player.getPassword()));
+            if (authenticaticon.isAuthenticated()) {
+                SecurityContextHolder.getContext().setAuthentication(authenticaticon);
+                return new ResponseEntity<>(player, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("User or Password Incorrect", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>("User or Password Incorrect", HttpStatus.BAD_REQUEST);
+    }
+}
--- giflex/src/main/java/br/ufsm/csi/poow2/giflex/security/WebSecurityConfig.java
+++ giflex/src/main/java/br/ufsm/csi/poow2/giflex/security/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package br.ufsm.csi.poow2.giflex.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,27 +29,42 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
     }
 
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(this.userDetailsService);
-        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-        return authenticationProvider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        System.out.println("*** WebSecurityConfig");
-        auth.authenticationProvider(this.authenticationProvider());
-    }
-
-
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(this.userDetailsService);
+//        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+//        return authenticationProvider;
+//    }
 
 
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests().antMatchers("/register**")
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        System.out.println("*** WebSecurityConfig");
+//        auth.authenticationProvider(this.authenticationProvider());
+//    }
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println("...auth manager configuration...");
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                //.authenticationProvider(this.authenticationProvider())
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/characters").permitAll() .anyRequest().authenticated();
+                //.and() .formLogin();
+
 //                .permitAll().anyRequest().authenticated()
 //                .and()
 //                .formLogin().loginPage("/login")
@@ -54,5 +72,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .and()
 //                .logout() .invalidateHttpSession(true)
 //                .clearAuthentication(true).permitAll();
-//    }
+    }
 }
